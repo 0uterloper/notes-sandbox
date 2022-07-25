e83acf8a25a83c9cfc07838e50136f6816946f38
Davis Foote <outerloper.ongoing@gmail.com>
2022-07-25 13:58:20 -0700

    Implement flat storage for notes on server. I have some writing about the decision-making process and the final design elsewhere; may put it somewhere visible at some point if it seems worthwhile.

--- local_sync/synchronizer.js
+++ local_sync/synchronizer.js
@@ -1,84 +1,48 @@
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
+const crypto = require('crypto')
 const bus = require('statebus').serve({file_store:false})
 bus.net_mount('/*', 'http://localhost:3006')
 
 const fs_root = '/Users/davisfoote/Documents/obsidian/Personal notes/'
 
-const save_file = (rel_path, create_parent_dirs = true) => {
-	if (rel_path === '.') {
-		// Special case at top of first recursive write.
-		bus.state['/notes'] = mkdir_if_undefined(bus.state['/notes'])
-		return
-	}
+const note_key_prefix = '/note/'
 
-	dir_obj = parse_dir_path_to_obj(path.dirname(rel_path), create_parent_dirs)
-	const filepath = path.join(fs_root, rel_path)
-	const basename = path.basename(filepath)
-	if (is_dir(filepath)) {
-		dir_obj.children[basename] = {
-			is_dir: true,
-			children: {},
-			content: null,
-		}
-			// mkdir_if_undefined(dir_obj.children[basename])
-	} else if (is_md(filepath)) {
-		// ^Slightly hacky placeholder logic for only wanting to sync .md files.
-		dir_obj.children[basename] = {
-			is_dir: false,
-			children: null,
-			content: fs.readFileSync(filepath, 'utf8'),
-		}
+const save_note = (rel_path) => {
+	const abs_path = path.join(fs_root, rel_path)
+
+	const key = note_key_prefix + hash_filepath(rel_path)
+	const content = fs.readFileSync(abs_path, 'utf8')
+
+	const note_obj = {
+		content: content,
+		location: rel_path,
 	}
+	bus.state[key] = note_obj
+	console.log(bus.state['/all_notes']())
+	bus.state['/all_notes'].notes[key] = note_obj
 }
 
-// Returns null if dir doesn't exist and create_parent_dirs is false.
-// Otherwise, returns the state object on server corresponding to that dir.
-const parse_dir_path_to_obj = (dir_path, create_parent_dirs = false) => {
-	var parent_obj = bus.state['/notes']
-	if (dir_path == '.') {
-		return parent_obj
-	}
-
-	dir_path.split(path.sep).forEach((child_dir) => {
-		if (create_parent_dirs) {
-			parent_obj.children[child_dir] = 
-				mkdir_if_undefined(parent_obj.children[child_dir])
-		} else if (!(child_dir in parent_obj)) {
-			return null
-		}
-		parent_obj = parent_obj.children[child_dir]
-	})
-
-	return parent_obj
+const hash_filepath = (rel_path) => {
+	// hex because base64 sometimes uses '/' and I feel like that could be a
+	// problem later.
+	return crypto.createHash('sha256').update(rel_path).digest('hex')
 }
 
-const mkdir_if_undefined = (x) => {
-	if (typeof x === 'undefined') {
-		return {
-			is_dir: true,
-			children: {},
-			content: null,
-		}
-	} else {
-		return x
-	}
-}
-
-// This implementation is a unnecessarily quadratic. This won't matter at all
-// at current scale, but could eventually.
-const recursive_save = (rel_path) => {
-	save_file(rel_path)
+const recursive_save = (rel_path = '.') => {
 	const abs_path = path.join(fs_root, rel_path)
 	if (is_dir(abs_path)) {
 		fs.readdirSync(abs_path).forEach(basename => {
-			if (!is_private(basename)) {
+			if (!is_private(basename)) {  // Ignore private directories.
 				recursive_save(path.join(rel_path, basename))
 			}
 		})
+	} else if (is_md(abs_path)) {
+		save_note(rel_path)
 	}
 }
+
 
 // Utils
 
@@ -90,14 +54,15 @@ const is_dir = (filepath) => {
 	return fs.lstatSync(filepath).isDirectory()
 }
 
-const is_private = filepath => Array.from(filepath)[0] === '.'
+const is_private = filepath => Array.from(path.basename(filepath))[0] === '.'
 
 const is_md = filepath => path.extname(filepath) === '.md'
 
 // Execution
 
 // Start with a full send over. This avoids more complicated diffing for now.
-recursive_save('.')
+bus.state['/all_notes'] = {notes: {}}
+recursive_save()
 
 // Watch source for changes and keep server synchronized with source.
 //
